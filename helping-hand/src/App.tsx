6645a52864608e49c3b0bfeb9ff72cc62e0d2571
roberts-garth <Garth.Roberts@LaunchPointFitness.io>
2022-11-12 15:23:02 -0800

    created helper and client profile components

--- helping-hand/src/App.tsx
+++ helping-hand/src/App.tsx
@@ -4,10 +4,16 @@ import './App.css';
 import Landing from "./components/landing/landing";
 import NavBar from "./components/navbar/navbar";
 import {Client} from "./models/client";
+import { Helper } from './models/helper';
+import ClientProfile from './components/client/clientProfile';
+import HelperProfile from './components/helper/helperProfile';
 // import Register from "./components/client-register/client-register";
 
 function App() {
-  const [authUser, setAuthUser] = useState<Client>();
+  const [loggedInHelper, setloggedInHelper] = useState<Helper>();
+  const [authUserHelper, setAuthUserHelper] = useState<Helper>();
+  const [authUserClient, setAuthUserClient] = useState<Client>();
+  const [loggedInClient, setLoggedInClient] = useState<Client>();
 
 
   return (
@@ -15,6 +21,8 @@ function App() {
       <NavBar />
       <Routes>
       <Route path="/" element={<Landing />}></Route>
+      <Route path={`/client/${loggedInClient?.id}`} element={<ClientProfile loggedInClient={loggedInClient}/>}></Route>
+      <Route path={`/helper/${loggedInHelper?.id}`} element={<HelperProfile loggedInHelper={loggedInHelper}/>}></Route>
       </Routes>
   </BrowserRouter>
   );
